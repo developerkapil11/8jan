a5a5cb0520181972eec412821987f7aaeea8993d
Kapil Hans <kplhans@gmail.com>
2024-01-09 14:05:07 +0530

    first commit from mac

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kapil Hans save to reload.
+          my name is Kapil Hans.
         </p>
         <a
           className="App-link"
